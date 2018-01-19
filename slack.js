a5dda19863582895f39e7729211bcb12d35c7173
Philip Scott <pscott@zeptohost.com>
2018-01-18 16:24:06 -0800

    feat(687): implement _notify for slack (#1)
    feat: implement _notify for slack
    BREAKING CHANGE: bump to version 1.0

---
+++ slack.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const hoek = require('hoek');
+const { WebClient } = require('@slack/client');
+
+let web;
+
+/**
+ * Retrieves the channel id
+ * @param {String} channelName             slack channel name
+ * @return {Promise}                       the channel id
+ */
+function getChannelId(channelName) {
+    const config = {
+        exclude_archived: true,
+        exclude_members: true
+    };
+
+    return web.channels.list(config)
+        .then(res => hoek.reach(res.channels.find(c => c.name === channelName), 'id'));
+}
+
+/**
+ * Post message to a specific channel
+ * @method postMessage
+ * @param {String} channelName      name of the channel
+ * @param {Object} payload          payload of the slack message
+ * @return {Promise}
+ */
+function postMessage(channelName, payload) {
+    return getChannelId(channelName)
+        .then((id) => {
+            if (!id) {
+                // eslint-disable-next-line no-console
+                throw new Error(`Channel ID not found for: ${channelName}`);
+            }
+
+            return web.chat.postMessage(id, payload.message, {
+                as_user: true,
+                attachments: payload.attachments
+            });
+        })
+        // eslint-disable-next-line no-console
+        .catch(err => console.error(err.message));
+}
+
+/**
+ * Sends slack message to slack channels
+ * @param {String} token                   access token for slack
+ * @param {String[]} channels              slack channel names
+ * @param {Object} payload
+ * @return {Promise}
+ */
+function slacker(token, channels, payload) {
+    if (!web) {
+        web = new WebClient(token);
+    }
+
+    return Promise.all(channels.map(channelName => postMessage(channelName, payload)));
+}
+
+module.exports = slacker;
---
+++ test/slack.test.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const assert = require('chai').assert;
+const mockery = require('mockery');
+const sinon = require('sinon');
+
+sinon.assert.expose(assert, { prefix: '' });
+describe('slack', () => {
+    let configMock;
+    let channels;
+    let payload;
+    let slacker;
+    let WebClientMock;
+    let WebClientConstructorMock;
+
+    before(() => {
+        mockery.enable({
+            useCleanCache: true,
+            warnOnUnregistered: false
+        });
+    });
+
+    beforeEach(() => {
+        WebClientMock = {
+            channels: {
+                list: sinon.stub().resolves({
+                    channels: [{
+                        name: 'meeseeks',
+                        id: '23'
+                    }, {
+                        name: 'dd',
+                        id: '21'
+                    }]
+                })
+            },
+            chat: {
+                postMessage: sinon.stub().resolves({})
+            }
+        };
+        WebClientConstructorMock = {
+            WebClient: sinon.stub().returns(WebClientMock)
+        };
+        mockery.registerMock('@slack/client', WebClientConstructorMock);
+
+        // eslint-disable-next-line global-require
+        slacker = require('../slack');
+    });
+
+    afterEach(() => {
+        mockery.deregisterAll();
+        mockery.resetCache();
+    });
+
+    after(() => {
+        mockery.disable();
+    });
+
+    describe('slacker posts message to channels', () => {
+        beforeEach(() => {
+            configMock = {
+                token: 'y353e5y45'
+            };
+            channels = ['meeseeks', 'caaaandoooo'];
+            payload = {
+                message: 'build failed',
+                attachments: {}
+            };
+        });
+
+        it('do not create client again if there is one', () =>
+            slacker(configMock.token, channels, payload)
+                .then(slacker(configMock.token, channels, payload))
+                .then(assert.calledOnce(WebClientConstructorMock.WebClient))
+        );
+
+        it('gets correct channel ids and post message to channels', () =>
+            slacker(configMock.token, channels, payload).then(() => {
+                assert.calledTwice(WebClientMock.channels.list);
+                assert.calledOnce(WebClientMock.chat.postMessage);
+                assert.calledWith(WebClientMock.chat.postMessage, '23', payload.message, {
+                    as_user: true,
+                    attachments: payload.attachments
+                });
+            })
+        );
+    });
+});
